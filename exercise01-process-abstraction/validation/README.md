2700d8f5621e0e95d0c3cb0b5387f7d5750667db
JREB <jreb1990@gmail.com>
2018-04-11 14:49:05 +0200

    Finished validation for exercise 01

--- exercise01-process-abstraction/validation/README.md
+++ exercise01-process-abstraction/validation/README.md
@@ -2,6 +2,7 @@
 
 A small website to automatically check if the exercise has been successfully solved.
 
-- Run `npm install` to install all dependencies.
-- Run `npm start` to host a local webserver.
+- Install nodejs (https://nodejs.org/en/download).
+- Run `npm install` in the directory of the `package.json` file to install all dependencies.
+- In the same directory, run `npm start` to host a local webserver.
 - Navigate to `http://localhost:5000` and click the `Start Validation` button.
--- exercise01-process-abstraction/validation/app/main.js
+++ exercise01-process-abstraction/validation/app/main.js
@@ -1,62 +1,117 @@
+Vue.component("test-results", {
+    props: ["test"],
+    template: "#results-component",
+    methods: {
+        hasFailed(results) {
+            return results.map(result => result.successful.expected === result.successful.achieved).includes(false);
+        }
+    }
+});
+
 const app = new Vue({
-    el: ".main",
+    el: "#main",
     data: {
-        message1: "",
-        validationFinished1: false,
-        message2: "",
-        validationFinished2: false,
-        creditRatingCheckResults: [],
-        productAvailabilityCheckResults: []
+        creditRatingTest: {
+            results: [],
+            tableHeaders: ["Customer ID", "Goal", "Status"],
+            idKey: "customerId",
+            message: "",
+            title: "Credit Rating Check Results",
+            validationFinished: false
+        },
+        productAvailabilityTest: {
+            results: [],
+            tableHeaders: ["Product ID", "Goal", "Status"],
+            idKey: "productId",
+            message: "",
+            title: "Product Availability Check Results",
+            validationFinished: false
+        },
+        orderTest: {
+            results: [],
+            tableHeaders: ["Order", "Goal", "Status"],
+            idKey: "orderDescription",
+            message: "",
+            title: "End-2-End Order Check Results",
+            validationFinished: false
+        }
     },
-    created() {
-        axios.get("app/data/credit-rating-checks.json").then(response => {
-            this.creditRatingCheckResults = response.data;
-        });
-        axios.get("app/data/product-availability-checks.json").then(response => {
-            this.productAvailabilityCheckResults = response.data;
-        });
-    },
+
     methods: {
 
         startValidation() {
             const productSrvEndpoint = "http://localhost:8000/products";
             const customerSrvEndpoint = "http://localhost:8010/customers";
             const orderSrvEndpoint = "http://localhost:8020/orders";
+            const marketingSrvEndpoint = "http://localhost:8030/marketing-mails";
 
             // test the customer credit rating check adjustments
-            this.validationFinished1 = false;
-            this.message1 = "Validation started...";
-            const customerSrvPromises = [];
-            this.creditRatingCheckResults.forEach(check => {
-                customerSrvPromises.push(axios.get(`${customerSrvEndpoint}/${check.customerId}/credit-rating-check`));
-            });
-            Promise.all(customerSrvPromises).then(results => {
-                results.forEach((result, index) => {
-                    this.creditRatingCheckResults[index].successful.achieved = result.data.acceptable;
+            axios.get("app/data/credit-rating-checks.json").then(response => {
+                const testSpecs = response.data;
+                this.creditRatingTest.validationFinished = false;
+                this.creditRatingTest.message = "Validation started...";
+                const customerSrvPromises = [];
+                testSpecs.forEach(check => {
+                    customerSrvPromises.push(axios.get(`${customerSrvEndpoint}/${check.customerId}/credit-rating-check`));
                 });
-                this.message1 = "Validation finished!";
-                this.validationFinished1 = true;
+                Promise.all(customerSrvPromises).then(results => {
+                    results.forEach((result, index) => {
+                        testSpecs[index].successful.achieved = result.data.acceptable;
+                    });
+                    this.creditRatingTest.results = testSpecs;
+                    this.creditRatingTest.message = "Validation finished!";
+                    this.creditRatingTest.validationFinished = true;
+                });
             });
+
 
             // test the product availability check adjustments
-            this.validationFinished2 = false;
-            this.message2 = "Validation started...";
-            const productSrvPromises = [];
-            this.productAvailabilityCheckResults.forEach(check => {
-                productSrvPromises.push(axios.get(`${productSrvEndpoint}/${check.productId}/availability?amount=${check.amount}`));
-            });
-            Promise.all(productSrvPromises).then(results => {
-                results.forEach((result, index) => {
-                    this.productAvailabilityCheckResults[index].successful.achieved = result.data.available;
+            axios.get("app/data/product-availability-checks.json").then(response => {
+                const testSpecs = response.data;
+                this.productAvailabilityTest.validationFinished = false;
+                this.productAvailabilityTest.message = "Validation started...";
+                const productSrvPromises = [];
+                testSpecs.forEach(check => {
+                    productSrvPromises.push(axios.get(`${productSrvEndpoint}/${check.productId}/availability?amount=${check.amount}`));
                 });
-                this.message2 = "Validation finished!";
-                this.validationFinished2 = true;
+                Promise.all(productSrvPromises).then(results => {
+                    results.forEach((result, index) => {
+                        testSpecs[index].successful.achieved = result.data.available;
+                    });
+                    this.productAvailabilityTest.results = testSpecs;
+                    this.productAvailabilityTest.message = "Validation finished!";
+                    this.productAvailabilityTest.validationFinished = true;
+                });
             });
-        },
 
-        hasFailed(results) {
-            return results.map(result => result.successful.expected === result.successful.achieved).includes(false);
+            // test order proccess adjustments end-2-end
+            axios.get("app/data/order-checks.json").then(response => {
+                const testSpecs = response.data;
+                this.orderTest.validationFinished = false;
+                this.orderTest.message = "Validation started...";
+                const orderSrvPromises = [];
+                testSpecs.forEach(check => {
+                    orderSrvPromises.push(axios.post(orderSrvEndpoint, check.order).catch(e => e));
+                });
+                Promise.all(orderSrvPromises).then(results => {
+                    results.forEach((result, index) => {
+                        if (result.response && result.response.status > 380) {
+                            // http error
+                            testSpecs[index].successful.achieved = false;
+                        } else {
+                            // http success, further check for marketing mail
+                            const orderId = parseInt(result.data.message.substring(result.data.message.indexOf("ID ") + 3, result.data.message.indexOf(" success")));
+                            axios.get(marketingSrvEndpoint).then(result => {
+                                result.data = result.data || [];
+                                testSpecs[index].successful.achieved = result.data.find(mail => mail.order.id === orderId) !== undefined;
+                            });
+                        }
+                    });
+                    this.orderTest.results = testSpecs;
+                    this.orderTest.message = "Validation finished!";
+                    this.orderTest.validationFinished = true;
+                });
+            });
         }
-
     }
 });
